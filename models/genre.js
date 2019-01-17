7ecc7a98581c71924d79c3bf7fc326837782a8f6
Raziur Rahman <instrumaniak@gmail.com>
2019-01-17 14:02:46 +0600

    added models

---
+++ models/genre.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const GenreSchema = new Schema({
+  name: {
+     type: String,
+     required: true,
+     min: 3,
+     max: 100 
+  }
+})
+
+// Virtual for genre's URL
+GenreSchema
+  .virtual('url')
+  .get(() => `/catalog/genre/${this._id}`)
+
+// Export model
+module.exports = mongoose.mode('Genre', GenreSchema)  
