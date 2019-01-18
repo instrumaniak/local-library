318206b8fe817e09c19e00962a09655acb60deb9
Raziur Rahman <instrumaniak@gmail.com>
2019-01-19 03:40:20 +0600

    Removed some stock cra files. Installed and setup bootstrap, reactstrap.

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <Button>Click me!!</Button>
+      </div>
+    )
+  }
+}
+
+export default App
