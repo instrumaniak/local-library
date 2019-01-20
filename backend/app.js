9f9011a8a8eca79d81ba65f2ad213ebf250b6b9b
Raziur Rahman <instrumaniak@gmail.com>
2019-01-20 17:34:59 +0600

    enabled cors for backend api

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 // import routes
 const indexRouter = require('./routes/index')
@@ -41,6 +42,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(cors())
 
 // serve static
 //app.use(express.static(path.join(__dirname, 'public')))
