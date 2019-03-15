247073d8cef4a46c824b644208f5ae39fd1b2e5d
Raziur Rahman <instrumaniak@gmail.com>
2019-03-16 02:24:54 +0600

    restyled SideNavbar & customized bootstrap primary color.

--- frontend/src/components/SideNavbar.js
+++ frontend/src/components/SideNavbar.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const SideNavBar = () => (
   <ul className="ll-sidenavbar">
-    <li><Link to="/">Local Library</Link></li>
+    <li className='ll-sidenavbar__title'><NavLink exact to="/">Local Library</NavLink></li>
 
-    <li><hr /></li>
-    <li><strong>Browse</strong></li>
-    <li><NavLink to="/catalog/books">Books</NavLink></li>
-    <li><NavLink to="/catalog/authors">Authors</NavLink></li>
-    <li><NavLink to="/catalog/genres">Genres</NavLink></li>
-    <li><NavLink to="/catalog/bookinstances">Book-instances</NavLink></li>
+    <li><div className='ll-sidenavbar__seperator'></div></li>
+    <li className='ll-sidenavbar__subtitle'><strong>Browse</strong></li>
+    <li className='ll-sidenavbar__link'><NavLink to="/catalog/books">Books</NavLink></li>
+    <li className='ll-sidenavbar__link'><NavLink to="/catalog/authors">Authors</NavLink></li>
+    <li className='ll-sidenavbar__link'><NavLink to="/catalog/genres">Genres</NavLink></li>
+    <li className='ll-sidenavbar__link'><NavLink to="/catalog/bookinstances">Book-instances</NavLink></li>
 
-    <li><hr /></li>
-    <li><strong>Create new</strong></li>
-    <li><NavLink to="/catalog/create/author">Author</NavLink></li>
-    <li><NavLink to="/catalog/create/genre">Genre</NavLink></li>
-    <li><NavLink to="/catalog/create/book">Book</NavLink></li>
-    <li><NavLink to="/catalog/create/bookinstance">Book instance</NavLink></li>
+    <li><div className='ll-sidenavbar__seperator'></div></li>
+    <li className='ll-sidenavbar__subtitle'><strong>Create new</strong></li>
+    <li className='ll-sidenavbar__link'><NavLink to="/catalog/create/author">Author</NavLink></li>
+    <li className='ll-sidenavbar__link'><NavLink to="/catalog/create/genre">Genre</NavLink></li>
+    <li className='ll-sidenavbar__link'><NavLink to="/catalog/create/book">Book</NavLink></li>
+    <li className='ll-sidenavbar__link'><NavLink to="/catalog/create/bookinstance">Book instance</NavLink></li>
   </ul>
 )
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 //import global styles
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/index.scss'
 
 import App from './components/App'
