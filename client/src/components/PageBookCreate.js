9bb263bbf138c2d1d6f517ba5843fb76e17030be
Raziur Rahman <instrumaniak@gmail.com>
2021-06-07 22:39:57 +0600

    FE: added book create page #5

--- client/src/components/PageBookCreate.js
+++ client/src/components/PageBookCreate.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import { URL } from '../services/api-endpoints'
 import { getData, postData } from '../services'
 import isEmpty from 'lodash.isempty'
@@ -9,11 +10,13 @@ const PageBookCreate = () => {
     author: '',
     summary: '',
     isbn: '',
-    genre: [],
   })
 
   const [authorList, setAuthorList] = useState([])
   const [genreList, setGenreList] = useState([])
+  const [hasError, setHasError] = useState(false)
+
+  const history = useHistory()
 
   const handleInputChange = (event) => {
     const { name, value } = event.target
@@ -21,17 +24,62 @@ const PageBookCreate = () => {
       ...formData,
       [name]: value,
     })
+    setHasError(false)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    postData(URL.book_create, {
+      title: formData.title,
+      author: formData.author,
+      summary: formData.summary,
+      isbn: formData.isbn,
+      genre: genreList
+        .filter((genre) => genre.checked)
+        .map((genre) => genre.id),
+    }).then((data) => {
+      if (isEmpty(data.errors)) {
+        setFormData({
+          title: '',
+          author: '',
+          summary: '',
+          isbn: '',
+        })
+        setGenreList(genreList.map((genre) => ({ ...genre, checked: false })))
+        history.push(`/catalog/book/${data.book_id}`)
+      } else {
+        setHasError(true)
+      }
+    })
+  }
+
+  const handleGenre = (id) => {
+    const genreIndex = genreList.findIndex((genre) => genre.id === id)
+    if (genreIndex > -1) {
+      const genreListNew = genreList.map((genre) =>
+        genre.id === id ? { ...genre, checked: !genre.checked } : genre
+      )
+      setGenreList(genreListNew)
+    }
   }
 
   useEffect(() => {
     getData(URL.authors).then((data) => setAuthorList(data.author_list))
-    getData(URL.genres).then((data) => setGenreList(data.genre_list))
+    getData(URL.genres).then((data) => {
+      setGenreList(
+        data.genre_list.map((genre) => ({
+          id: genre.id,
+          name: genre.name,
+          checked: false,
+        }))
+      )
+    })
   }, [])
 
   return (
     <div>
       <h1>Create new book</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Title:</label>
           <input
@@ -50,8 +98,11 @@ const PageBookCreate = () => {
             value={formData.author}
             onChange={handleInputChange}
           >
-            {authorList.map((author) => (
-              <option value={author.id}>{author.name}</option>
+            <option value="">Please Select</option>
+            {authorList.map((author, index) => (
+              <option value={author.id} key={index}>
+                {author.name}
+              </option>
             ))}
           </select>
         </div>
@@ -76,9 +127,14 @@ const PageBookCreate = () => {
         <br />
         <label>Genre:</label>
         <div>
-          {genreList.map((genre) => (
-            <label>
-              <input type="checkbox" /> {genre.name} &nbsp;&nbsp;
+          {genreList.map((genre, index) => (
+            <label key={index}>
+              <input
+                type="checkbox"
+                checked={genre.checked}
+                onChange={() => handleGenre(genre.id)}
+              />{' '}
+              {genre.name} &nbsp;&nbsp;
             </label>
           ))}
         </div>
@@ -87,6 +143,7 @@ const PageBookCreate = () => {
           Submit
         </button>
       </form>
+      {hasError && <span>Error occured</span>}
     </div>
   )
 }
