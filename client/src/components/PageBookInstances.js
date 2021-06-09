54e4de4556bbac4e5b12ee202557cfcb13c03cff
Raziur Rahman <instrumaniak@gmail.com>
2021-06-09 23:20:50 +0600

    FE: bootstrap 4 to 5 migration fixes #8

--- client/src/components/PageBookInstances.js
+++ client/src/components/PageBookInstances.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Badge } from 'reactstrap'
+// import { Badge } from 'reactstrap'
 import isEmpty from 'lodash.isempty'
 import { Link } from 'react-router-dom'
 import { getData } from '../services'
@@ -8,44 +8,47 @@ import { URL } from '../services/api-endpoints'
 class PageBookInstances extends Component {
   state = {
     title: '',
-    bookinstance_list: []
+    bookinstance_list: [],
   }
   componentDidMount() {
-    getData(URL.bookinstances)
-      .then(data => this.setState(data))
+    getData(URL.bookinstances).then((data) => this.setState(data))
   }
   render() {
     const { title, bookinstance_list } = this.state
 
-    const activeColor = status => {
-      switch(status) {
+    const badgeColorClass = (status) => {
+      switch (status) {
         case 'Available':
-          return 'success'
+          return 'bg-success'
         case 'Maintenance':
-          return 'danger'
+          return 'bg-danger'
         default:
-          return 'warning'
+          return 'bg-warning'
       }
     }
 
     return (
       <div>
-        <h1>{ title }</h1>
+        <h1>{title}</h1>
         <ul>
-          { !isEmpty(bookinstance_list) &&
-             bookinstance_list.map((item, id) => (
+          {!isEmpty(bookinstance_list) &&
+            bookinstance_list.map((item, id) => (
               <li key={id}>
                 <Link to={item.url}>{item.book.title}</Link>
-                <span>{' '}<Badge color={activeColor(item.status)}>{item.status}</Badge></span>
+                <span className={`ms-2 badge ${badgeColorClass(item.status)}`}>
+                  {item.status}
+                </span>
                 <div>{item.imprint}</div>
-                <div>{
-                  `${item.status !== 'Available' ? 'Due: ' + item.due_back_formatted : ''}`
-                }</div>
+                <div>{`${
+                  item.status !== 'Available'
+                    ? 'Due: ' + item.due_back_formatted
+                    : ''
+                }`}</div>
               </li>
-          ))}
-          { isEmpty(bookinstance_list) &&
-              <li>There are not book copies in this library.</li>
-          }
+            ))}
+          {isEmpty(bookinstance_list) && (
+            <li>There are not book copies in this library.</li>
+          )}
         </ul>
       </div>
     )
