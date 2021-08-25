d7ea40b2fb3cd5d29c3ae8b1af000a0fa7d5c57e
Raziur Rahman <instrumaniak@gmail.com>
2021-08-25 17:11:26 +0600

    BE: setup swagger & added docs for users section #17

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,35 @@
 const router = require('express').Router()
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsdoc = require('swagger-jsdoc')
+
 const usersRouter = require('./users')
 const catalogRouter = require('./catalog')
 
-router.use('/catalog', catalogRouter).use('/users', usersRouter)
+const options = {
+  definition: {
+    swagger: '2.0',
+    info: {
+      title: 'LocalLibrary',
+      version: '1.0.0',
+      description: 'API Documentation',
+    },
+    basePath: '/api',
+  },
+  apis: ['./routes/*.js'], // files containing annotations as above
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Users
+ *     description: User login/signup & other user management
+ */
+
+router
+  .use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+  .use('/catalog', catalogRouter)
+  .use('/users', usersRouter)
 
 module.exports = router
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,13 +3,82 @@ const router = express.Router()
 const { requiresAuthentication } = require('../middlewares/auth')
 const userController = require('../controllers/userController')
 
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     summary: Register a new user
+ *     description: Register a new user
+ *     tags: [Users]
+ *     consumes:
+ *       -  application/json
+ *     produces:
+ *       -  application/json
+ *     parameters:
+ *       -  in: body
+ *          name: body
+ *          description: User registration Object
+ *          required: true
+ *          type: object
+ *          properties:
+ *            name:
+ *              type: string
+ *            username:
+ *              type: string
+ *            email:
+ *              type: string
+ *            password:
+ *              type: string
+ *            role:
+ *              type: string
+ *     responses:
+ *       201:
+ *         description: Registration successful
+ */
 router.post('/register', userController.register)
+
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: User login, to get new JWT Token
+ *     description: Submit user login data to get JWT Token
+ *     tags: [Users]
+ *     consumes:
+ *       -  application/json
+ *     produces:
+ *       -  application/json
+ *     parameters:
+ *       -  in: body
+ *          name: payload
+ *          required: true
+ *          type: object
+ *          properties:
+ *            username:
+ *              type: string
+ *            password:
+ *              type: string
+ *     responses:
+ *       200:
+ *         description: Returns a JWT token data
+ */
 router.post('/login', userController.login)
 
 /**
  *  Get authenticated user profile
  */
 
+/**
+ * @swagger
+ * /users/profile:
+ *   get:
+ *     summary: Get user profile data
+ *     description: Get user profile data
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Returns a User profile data
+ */
 router.get('/profile', requiresAuthentication, userController.profile)
 
 module.exports = router
